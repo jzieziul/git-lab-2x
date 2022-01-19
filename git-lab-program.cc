39b1870714fa620a001004b82d386ca885ba76b9
Jackson Zieziul <jz334520@ohio.edu>
2022-01-19 18:45:35 -0500

    Lab program

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Jackson Zieziul
+ *        Date: 1-19-22
+ * Description: Simple test program
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
